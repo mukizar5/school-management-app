d71939436caa9215e5323318f8b419dcda592f2a
Mukiza Raymond <mukizar5@gmail.com>
2025-05-19 16:02:55 +0300

    Add files via upload

---
+++ Repositories/AssignmentResults/IAssignmentResultsRepository.cs
@@ -0,0 +1,9 @@
+using SchoolManagementSystem.Api.Models;
+using SchoolManagementSystem.Api.Repositories.Generic;
+
+namespace SchoolManagementSystem.Api.Repositories.AssignmentResults;
+
+public interface IAssignmentResultsRepository: IGenericRepository<AssignmentResult>
+{
+    
+}
